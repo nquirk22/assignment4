4eb827a3d1ab07efb1be2bc8edaa425f0721d7c7
Nathan Quirk <nquirk22@gmail.com>
2018-10-08 01:31:55 -0400

    updated profile.py

--- profile.py
+++ profile.py
@@ -37,22 +37,6 @@ prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
 for i in range(14):
-
-  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
-  
-  iface = node.addInterface("if" + str(i))
-  iface.component_id = "eth1"
-  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-  link.addInterface(iface)
-  
-  node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/passwordless.sh"))
-  node.addService(pg.Execute(shell="sh", command="sudo /local/repository/passwordless.sh"))  
-  
-  # This code segment is added per Benjamin Walker's solution to address the StrictHostKeyCheck issue of ssh
-  node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/ssh_setup.sh"))
-  node.addService(pg.Execute(shell="sh", command="sudo -H -u nq666287 bash -c '/local/repository/ssh_setup.sh'"))
- 
-  node.addService(pg.Execute(shell="sh", command="sudo su nq666287 -c 'cp /local/repository/source/* /users/nq666287'"))
   
   if i == 0:
     node = request.XenVM("head")
@@ -139,6 +123,22 @@ for i in range(14):
     # Add MPI to PATH
     node.addService(pg.Execute(shell="sh", command="sudo echo 'export PATH=/software:$PATH' >> ~/.bashrc"))
     node.addService(pg.Execute(shell="sh", command="source ~/.bashrc"))
+    
+    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+  
+  iface = node.addInterface("if" + str(i))
+  iface.component_id = "eth1"
+  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+  link.addInterface(iface)
+  
+  node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/passwordless.sh"))
+  node.addService(pg.Execute(shell="sh", command="sudo /local/repository/passwordless.sh"))  
+  
+  # This code segment is added per Benjamin Walker's solution to address the StrictHostKeyCheck issue of ssh
+  node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/ssh_setup.sh"))
+  node.addService(pg.Execute(shell="sh", command="sudo -H -u nq666287 bash -c '/local/repository/ssh_setup.sh'"))
+ 
+  node.addService(pg.Execute(shell="sh", command="sudo su nq666287 -c 'cp /local/repository/source/* /users/nq666287'"))
     
   
   
