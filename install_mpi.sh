a0518ba35b7b441af205de85c80a851b02eef59c
Nathan Quirk <41979183+nquirk22@users.noreply.github.com>
2018-10-08 16:10:40 -0400

    Update install_mpi.sh

--- install_mpi.sh
+++ install_mpi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
 sudo yum -y group install "Development Tools"
+cd /software
 sudo wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.2.tar.gz
 sudo tar xzf openmpi-3.1.2.tar.gz
 cd openmpi-3.1.2
